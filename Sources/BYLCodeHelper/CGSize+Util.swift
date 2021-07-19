695b9c9d9f3529e537f4a653ac4ca62f3485f15b
Ben LIU <arkilis@gmail.com>
2021-07-20 08:30:03 +1000

    add cgsize

--- Sources/BYLCodeHelper/CGSize+Util.swift
+++ Sources/BYLCodeHelper/CGSize+Util.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
+//  CGSize+Util.swift
 //  
 //
 //  Created by Ben Liu on 20/7/21.
 //
 
-import Foundation
+import UIKit
+
+public extension CGSize {
+  
+  func scale(to: Float) ->  CGSize {
+    return CGSize.init(width: self.width * CGFloat(to), height: self.height * CGFloat(to))
+  }
+  
+}
